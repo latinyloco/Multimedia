bc92bc730fbcaf4bc77ac2432262ec661c798e8e
AmandaCastro <103392535+latinyloco@users.noreply.github.com>
2024-02-05 20:14:54 +0100

    Orange mostrar

--- MundoBasico_Pruebas/MundoBasico_Pruebas/Prueba_6 PanningAroundLevel/js/game.js
+++ MundoBasico_Pruebas/MundoBasico_Pruebas/Prueba_6 PanningAroundLevel/js/game.js
@@ -1,158 +1,168 @@
 // Solicitud de requestAnimationFrame y cancelAnimationFrame para su uso en el código del juego
-(function() {
+(function () {
     var lastTime = 0;
     var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = 
-        window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+            window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
     }
- 
+
     if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
+        window.requestAnimationFrame = function (callback, element) {
             var currTime = new Date().getTime();
             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
+            var id = window.setTimeout(function () { callback(currTime + timeToCall); },
+                timeToCall);
             lastTime = currTime + timeToCall;
             return id;
         };
- 
+
     if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
+        window.cancelAnimationFrame = function (id) {
             clearTimeout(id);
         };
 }());
 
-$(window).load(function() {
-	game.init();
+$(window).load(function () {
+    game.init();
 });
 
 var game = {
-   // Comenzar inicialización de objetos, precarga de elementos y pantalla de inicio
-    init: function(){
+    // Comenzar inicialización de objetos, precarga de elementos y pantalla de inicio
+    init: function () {
         // Inicializar objetos   
         levels.init();
         loader.init();
         mouse.init();
-        
+
         // Ocultar todas las capas del juego y mostrar la pantalla de inicio
         $('.gamelayer').hide();
         $('#gamestartscreen').show();
-        
+
         //Obtener manejador para el canvas del juego y el contexto
         game.canvas = $('#gamecanvas')[0];
         game.context = game.canvas.getContext('2d');
     },
 
-    showLevelScreen:function(){
+    showLevelScreen: function () {
         $('.gamelayer').hide();
         $('#levelselectscreen').show('slow');
     },
 
     // Modo Juego 
-    mode:"intro", 
+    mode: "intro",
     // Coordenadas X & Y de la honda
-    slingshotX:140,
-    slingshotY:280,
-    
-    start:function(){
+    slingshotX: 140,
+    slingshotY: 280,
+
+    start: function () {
         $('.gamelayer').hide();
         // Display the game canvas and score 
         $('#gamecanvas').show();
         $('#scorescreen').show();
-    
-        game.mode = "intro";    
+
+        game.mode = "intro";
         game.offsetLeft = 0;
         game.ended = false;
-        game.animationFrame = window.requestAnimationFrame(game.animate,game.canvas);
-    },  
+        game.animationFrame = window.requestAnimationFrame(game.animate, game.canvas);
+    },
 
     // Velocidad máxima de panoramización por fotograma en píxeles
-    maxSpeed:3,
+    maxSpeed: 3,
     // Mínimo y Máximo desplazamiento panorámico
-    minOffset:0,
-    maxOffset:300,
+    minOffset: 0,
+    maxOffset: 300,
     // Desplazamiento de panorámica actual
-    offsetLeft:0,
+    offsetLeft: 0,
     // La puntuación del juego
-    score:0,
+    score: 0,
 
 
     //Despliegue la pantalla para centrarse en newCenter
-    panTo:function(newCenter){
-        if (Math.abs(newCenter-game.offsetLeft-game.canvas.width/4)>0 
-            && game.offsetLeft <= game.maxOffset && game.offsetLeft >= game.minOffset){
-        
-            var deltaX = Math.round((newCenter-game.offsetLeft-game.canvas.width/4)/2);
-            if (deltaX && Math.abs(deltaX)>game.maxSpeed){
-                deltaX = game.maxSpeed*Math.abs(deltaX)/(deltaX);
+    panTo: function (newCenter) {
+        if (Math.abs(newCenter - game.offsetLeft - game.canvas.width / 4) > 0
+            && game.offsetLeft <= game.maxOffset && game.offsetLeft >= game.minOffset) {
+
+            var deltaX = Math.round((newCenter - game.offsetLeft - game.canvas.width / 4) / 2);
+            if (deltaX && Math.abs(deltaX) > game.maxSpeed) {
+                deltaX = game.maxSpeed * Math.abs(deltaX) / (deltaX);
             }
-            game.offsetLeft += deltaX; 
+            game.offsetLeft += deltaX;
         } else {
-            
+
             return true;
         }
-        if (game.offsetLeft <game.minOffset){
+        if (game.offsetLeft < game.minOffset) {
             game.offsetLeft = game.minOffset;
             return true;
-        } else if (game.offsetLeft > game.maxOffset){
+        } else if (game.offsetLeft > game.maxOffset) {
             game.offsetLeft = game.maxOffset;
             return true;
-        }       
+        }
         return false;
     },
 
-        handlePanning:function(){
-           if(game.mode=="intro"){      
-               if(game.panTo(700)){
-                   game.mode = "load-next-hero";
-               }             
-           }       
+    handlePanning: function () {
+        if (game.mode == "intro") {
+            if (game.panTo(700)) {
+                game.mode = "load-next-hero";
+            }
+        }
 
-           if (game.mode == "wait-for-firing"){  
-            if (mouse.dragging){
-                    game.panTo(mouse.x + game.offsetLeft)
+        if (game.mode == "wait-for-firing") {
+            if (mouse.dragging) {
+                game.panTo(mouse.x + game.offsetLeft)
             } else {
                 game.panTo(game.slingshotX);
-              }
             }
+        }
 
-        if(game.mode == "load-next-hero"){
+        if (game.mode == "load-next-hero") {
             //TODO:
             //Verificar si los vilanos están vivos, si no terminar el nivel (éxito)
             //Verificar si faltan héroes por cargar, si no terminar el nivel (fracaso)
             //Cargar el héroe y fijar el modo a "wait-for-firing" 
-            game.mode =="wait-for-firing";
+            game.mode == "wait-for-firing";
         }
 
-        if (game.mode == "firing"){  
-                game.panTo(game.slingshotX);                
-        } 
+        if (game.mode == "firing") {
+            game.panTo(game.slingshotX);
+        }
 
-        if (game.mode == "fired"){
-                //TODO:
-                //Hacer un barrido hasta donde se encuentra el héroe actualmente
+        if (game.mode == "fired") {
+            //TODO:
+            //Hacer un barrido hasta donde se encuentra el héroe actualmente
         }
     },
 
-       animate:function(){
+    animate: function () {
         // Animar el fondo
         game.handlePanning();
 
         // Animar los personajes
-
         
+
         // Dibujar el fondo con desplazamiento de paralaje
-        game.context.drawImage(game.currentLevel.backgroundImage,game.offsetLeft/4,0,640,480,0,0,640,480);
-        game.context.drawImage(game.currentLevel.foregroundImage,game.offsetLeft,0,640,480,0,0,640,480);
+        game.context.drawImage(game.currentLevel.backgroundImage, game.offsetLeft / 4, 0, 640, 480, 0, 0, 640, 480);
+        game.context.drawImage(game.currentLevel.foregroundImage, game.offsetLeft, 0, 640, 480, 0, 0, 640, 480);
+
 
         // Dibujar la honda
-        game.context.drawImage(game.slingshotImage,game.slingshotX-game.offsetLeft,game.slingshotY);
+        game.context.drawImage(game.slingshotImage, game.slingshotX - game.offsetLeft, game.slingshotY);
 
-        if (!game.ended){
-            game.animationFrame = window.requestAnimationFrame(game.animate,game.canvas);
-        } 
+
+        //CARGAR HEROES
+        for (var i = 0; i < 1; i++) {
+            var entity = game.currentLevel.hero[i];
+            console.log(entity);
+            game.context.drawImage(entity, entity.x - game.offsetLeft, entity.y);
+        }
+
+
+        if (!game.ended) {
+            game.animationFrame = window.requestAnimationFrame(game.animate, game.canvas);
+        }
     },
 
 }
@@ -160,69 +170,88 @@ var game = {
 
 var levels = {
     // Datos del nivel
-    data:[
-     {// Primer nivel
-        foreground:'desert-foreground',
-        background:'clouds-background',
-        entities:[]
-     },
-     { // Segundo nivel
-        foreground:'desert-foreground',
-        background:'clouds-background',
-        entities:[]
-     }
+    data: [
+        {// Primer nivel
+            foreground: 'desert-foreground',
+            background: 'clouds-background',
+            entities: [
+                { type: 'enemy', x: 1000, y: 1000, image: 'images/entities/pizza.png' },
+                { type: 'heroe', x: 10000, y: 10000, image: 'images/entities/orange.png' }
+            ]
+        },
+        { // Segundo nivel
+            foreground: 'desert-foreground',
+            background: 'clouds-background',
+            entities: [
+                { type: 'enemy', x: 500, y: 500, image: 'images/entities/pizza.png' },
+                { type: 'heroe', x: 400, y: 400, image: 'images/entities/orange.png' }
+            ]
+        }
     ],
 
     // Inicializar pantalla de selección de nivel
-    init:function(){
+    init: function () {
         var html = "";
-        for (var i=0; i < levels.data.length; i++) {
+        for (var i = 0; i < levels.data.length; i++) {
             var level = levels.data[i];
-            html += '<input type="button" value="'+(i+1)+'">';
+            html += '<input type="button" value="' + (i + 1) + '">';
         };
         $('#levelselectscreen').html(html);
-        
+
         // Establecer los controladores de eventos de clic de botón para cargar el nivel
-        $('#levelselectscreen input').click(function(){
-            levels.load(this.value-1);
+        $('#levelselectscreen input').click(function () {
+            levels.load(this.value - 1);
             $('#levelselectscreen').hide();
         });
     },
 
     // Cargar todos los datos e imágenes para un nivel específico
-    load:function(number){
+    load: function (number) {
 
         //Declarar un nuevo objeto de nivel actual
-        game.currentLevel = {number:number,hero:[]};
-        game.score=0;
-        $('#score').html('Score: '+game.score);
-        game.currentHero = undefined;
+        game.currentLevel = { number: number, hero: [] };
+        game.score = 0;
+        $('#score').html('Score: ' + game.score);
+        game.currentHero = 1;
         var level = levels.data[number];
 
-
+        
         //Cargar las imágenes de fondo, primer plano y honda
-        game.currentLevel.backgroundImage = loader.loadImage("images/backgrounds/"+level.background+".png");
-        game.currentLevel.foregroundImage = loader.loadImage("images/backgrounds/"+level.foreground+".png");
+        game.currentLevel.backgroundImage = loader.loadImage("images/backgrounds/" + level.background + ".png");
+        game.currentLevel.foregroundImage = loader.loadImage("images/backgrounds/" + level.foreground + ".png");
         game.slingshotImage = loader.loadImage("images/slingshot.png");
         game.slingshotFrontImage = loader.loadImage("images/slingshot-front.png");
 
-       //Llamar a game.start() una vez que todos los assets han sido cargados
-       if(loader.loaded){
-           game.start()
-       } else {
-           loader.onload = game.start;
-       }
+
+        //CARGAR ENTIDADES
+        var j = 0;
+        for (var i = 0; i < level.entities.length; i++) {
+            var entity = level.entities[i];
+            if(level.entities[i].type == "heroe"){
+                game.currentLevel.hero[j] = loader.loadImage(level.entities[i].image);
+                j++;
+            }
+
+        }
+
+        //Llamar a game.start() una vez que todos los assets han sido cargados
+        if (loader.loaded) {
+            
+            game.start()
+        } else {
+            loader.onload = game.start;
+        }
     }
 }
 
 var loader = {
-    loaded:true,
-    loadedCount:0, // Assets que se han cargado hasta ahora
-    totalCount:0, // Número total de Assets que deben cargarse
-    
-    init:function(){
+    loaded: true,
+    loadedCount: 0, // Assets que se han cargado hasta ahora
+    totalCount: 0, // Número total de Assets que deben cargarse
+
+    init: function () {
         // Comprobar si hay soporte de sonido
-        var mp3Support,oggSupport;
+        var mp3Support, oggSupport;
         var audio = document.createElement('audio');
         if (audio.canPlayType) {
             // Actualmente canPlayType() devuelve: "", "maybe" o "probably" 
@@ -231,14 +260,14 @@ var loader = {
         } else {
             //La etiqueta de audio no es soportada
             mp3Support = false;
-            oggSupport = false; 
+            oggSupport = false;
         }
 
         // Comprobar para ogg, después mp3, y finalmente fije soundFileExtn a indefinido
-        loader.soundFileExtn = oggSupport?".ogg":mp3Support?".mp3":undefined;        
+        loader.soundFileExtn = oggSupport ? ".ogg" : mp3Support ? ".mp3" : undefined;
     },
-    
-    loadImage:function(url){
+
+    loadImage: function (url) {
         this.totalCount++;
         this.loaded = false;
         $('#loadingscreen').show();
@@ -247,26 +276,26 @@ var loader = {
         image.onload = loader.itemLoaded;
         return image;
     },
-    soundFileExtn:".ogg",
-    loadSound:function(url){
+    soundFileExtn: ".ogg",
+    loadSound: function (url) {
         this.totalCount++;
         this.loaded = false;
         $('#loadingscreen').show();
         var audio = new Audio();
-        audio.src = url+loader.soundFileExtn;
+        audio.src = url + loader.soundFileExtn;
         audio.addEventListener("canplaythrough", loader.itemLoaded, false);
-        return audio;   
+        return audio;
     },
-    itemLoaded:function(){
+    itemLoaded: function () {
         loader.loadedCount++;
-        $('#loadingmessage').html('Loaded '+loader.loadedCount+' of '+loader.totalCount);
-        if (loader.loadedCount === loader.totalCount){
+        $('#loadingmessage').html('Loaded ' + loader.loadedCount + ' of ' + loader.totalCount);
+        if (loader.loadedCount === loader.totalCount) {
             // Loader has loaded completely..
             loader.loaded = true;
             // Hide the loading screen 
             $('#loadingscreen').hide();
             //Y llamar al método loader.onload si existe
-            if(loader.onload){
+            if (loader.onload) {
                 loader.onload();
                 loader.onload = undefined;
             }
@@ -275,33 +304,33 @@ var loader = {
 }
 
 var mouse = {
-    x:0,
-    y:0,
-    down:false,
-    init:function(){
+    x: 0,
+    y: 0,
+    down: false,
+    init: function () {
         $('#gamecanvas').mousemove(mouse.mousemovehandler);
         $('#gamecanvas').mousedown(mouse.mousedownhandler);
         $('#gamecanvas').mouseup(mouse.mouseuphandler);
         $('#gamecanvas').mouseout(mouse.mouseuphandler);
     },
-    mousemovehandler:function(ev){
+    mousemovehandler: function (ev) {
         var offset = $('#gamecanvas').offset();
-        
+
         mouse.x = ev.pageX - offset.left;
         mouse.y = ev.pageY - offset.top;
-        
+
         if (mouse.down) {
             mouse.dragging = true;
         }
     },
-    mousedownhandler:function(ev){
+    mousedownhandler: function (ev) {
         mouse.down = true;
         mouse.downX = mouse.x;
         mouse.downY = mouse.y;
         ev.originalEvent.preventDefault();
-        
+
     },
-    mouseuphandler:function(ev){
+    mouseuphandler: function (ev) {
         mouse.down = false;
         mouse.dragging = false;
     }
